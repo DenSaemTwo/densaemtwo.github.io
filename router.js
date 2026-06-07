03b8d6c516bbd9f9b139b66ee22dca337c7f9899
EBAHYTbI ДEД <lakamoikusok2006@gmail.com>
2026-06-07 21:59:08 +0300

    Update router.js
    Опять снёс алерты отсюда

--- router.js
+++ router.js
@@ -1,18 +1,3 @@
-window.alert = function(message) {
-    const modal = document.getElementById("customAlertModal");
-    const msgElem = document.getElementById("customAlertMessage");
-    if (modal && msgElem) {
-        msgElem.textContent = message;
-        modal.style.display = "flex";
-    } else {
-        console.warn("Предупреждение:", message);
-    }
-};
-
-function closeCustomAlert() {
-    const modal = document.getElementById("customAlertModal");
-    if (modal) modal.style.display = "none";
-}
 
 const router = {
     navigate(view) {
